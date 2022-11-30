7ebf1295e4d3940ecf38f7b892fd507544332537
Kacper_M <74098084+Kacper-017@users.noreply.github.com>
2022-11-30 13:27:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,7 @@ st.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
